4c8797357a98b6c25c8e74e072199374b5391777
Andrew Taylor <ataylor@redtoad.ca>
2016-01-20 16:56:59 -0700

    Change emacs package repo from marmalade to melpa-stable.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -86,7 +86,8 @@
 (load "~/.emacs.d/string-inflection")
 
 (require 'package)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
 
 (projectile-global-mode)
