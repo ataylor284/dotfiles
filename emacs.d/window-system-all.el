18859eff7c910ce45ce51eaa751d0236fe83a041
Andrew Taylor <ataylor@redtoad.ca>
2016-02-18 23:20:26 -0700

    Cleaned up windowing system settings: start in fullscreen mode, and set color/font defaults for all frames instead of just the initial one.

--- emacs.d/window-system-all.el
+++ emacs.d/window-system-all.el
@@ -1,8 +1,14 @@
 ;; GUI mode UI settings
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(set-foreground-color "green")
-(set-background-color "black")
-(setq x-select-enable-clipboard t)
 (blink-cursor-mode -1)
-(setq visible-bell 1)
+
+(setq x-select-enable-clipboard t
+      visible-bell 1)
+
+(add-to-list 'default-frame-alist '(foreground-color . "green"))
+(add-to-list 'default-frame-alist '(background-color . "black"))
+(add-to-list 'default-frame-alist '(cursor-color . "white"))
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+(add-hook 'window-setup-hook 'toggle-frame-fullscreen t)
--- emacs.d/window-system-ns.el
+++ emacs.d/window-system-ns.el
@@ -1,6 +1,7 @@
 ;; OS X
-
 (setq exec-path (append exec-path '("/usr/local/bin" "/usr/X11/bin")))
-(set-default-font "Menlo-15")
+
+(add-to-list 'default-frame-alist '(font . "Menlo-15"))
+
 (setq save-interprogram-paste-before-kill nil
       mac-command-modifier 'meta)
