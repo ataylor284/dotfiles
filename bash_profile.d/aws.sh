7f63e161b9bb263b1955aa0a158f4497b10fec89
Andrew Taylor <ataylor@redtoad.ca>
2016-03-03 15:13:21 -0700

    Updated aws CLI aliases.

--- bash_profile.d/aws.sh
+++ bash_profile.d/aws.sh
@@ -3,5 +3,12 @@ alias aws_ec2_ls="aws ec2 describe-instances --query 'Reservations[].Instances[]
 
 # Cloud Formation
 alias aws_cf_ls="aws cloudformation describe-stacks --query=Stacks[].[Description,StackName,StackStatus] --output=table"
+alias aws_cf_params="aws cloudformation describe-stacks --query=Stacks[].[Parameters] --output=table --stack-name"
 alias aws_cf_outputs="aws cloudformation describe-stacks --query=Stacks[].[Outputs] --output=table --stack-name"
-alias aws_cf_resources="aws cloudformation describe-stack-resources --query StackResources[].[LogicalResourceId,ResourceType,ResourceStatus] --output table --stack-name"
+alias aws_cf_resources="aws cloudformation describe-stack-resources --query StackResources[].[LogicalResourceId,PhysicalResourceId,ResourceType,ResourceStatus] --output table --stack-name"
+
+# Lambda
+alias aws_lm_ls="aws lambda list-functions --query Functions[].[FunctionName,Runtime,LastModified] --output table"
+alias aws_lm_hash="aws lambda list-functions --query Functions[].[FunctionName,CodeSha256] --output table"
+alias aws_lm_versions="aws lambda list-versions-by-function --query Versions[].[Version,LastModified] --output table --function-name"
+alias aws_lm_aliases="aws lambda list-aliases --query Aliases[].[Name,FunctionVersion] --output table --function-name"
