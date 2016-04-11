c3a63f40385a3a0411fa0c3ef688a71ddf2a8015
Andrew Taylor <ataylor@redtoad.ca>
2016-04-11 09:29:30 -0600

    More aws aliases.

--- bash_profile.d/aws.sh
+++ bash_profile.d/aws.sh
@@ -12,6 +12,8 @@ alias aws_cf_params="aws cloudformation describe-stacks --query=Stacks[].[Parame
 alias aws_cf_outputs="aws cloudformation describe-stacks --query=Stacks[].[Outputs] --output=table --stack-name"
 alias aws_cf_resources="aws cloudformation describe-stack-resources --query StackResources[].[LogicalResourceId,PhysicalResourceId,ResourceType,ResourceStatus] --output table --stack-name"
 alias aws_cf_events="aws cloudformation describe-stack-events --query StackEvents[].[Timestamp,ResourceStatus,LogicalResourceId,ResourceType] --output table --stack-name"
+alias aws_cf_rm="aws cloudformation delete-stack --stack-name"
+aws_cf_resource_name() { aws cloudformation describe-stack-resources --stack-name $1 --logical-resource-id $2 --query StackResources[].[PhysicalResourceId] --output text; }
 
 # Lambda
 alias aws_lm_ls="aws lambda list-functions --query Functions[].[FunctionName,Runtime,LastModified] --output table"
@@ -29,9 +31,27 @@ aws_dy_nth_row() { aws dynamodb scan --max-items $(($2 + 1)) --query 'Items['$2'
 # Kinesis Firehose
 alias aws_fh_ls="aws firehose list-delivery-streams --limit=200 --query DeliveryStreamNames[][@] --output text"
 alias aws_fh_dests="aws firehose describe-delivery-stream --query DeliveryStreamDescription.Destinations --output table --delivery-stream-name"
+alias aws_fh_rm="aws firehose delete-delivery-stream --delivery-stream-name"
 
 # S3
 alias aws_s3_stack="aws s3api get-bucket-tagging --query 'TagSet[?Key==\`aws:cloudformation:stack-name\`] | [0].Value' --output text --bucket"
 
 # Cloud Trail
 alias aws_ct_events="aws cloudtrail lookup-events --max-results 15 --query Events[].[EventTime,Username,EventName] --output table"
+
+# Events
+alias aws_ev_ls="aws events list-rules --query Rules[].[Name,ScheduleExpression,State] --output table"
+alias aws_ev_targets="aws events list-targets-by-rule --query Targets[].[Id,Arn] --output table --rule"
+aws_ev_rm_target() { aws events remove-targets --rule $1 --ids $2 --output table; }
+alias aws_ev_rm="aws events delete-rule --name"
+
+# Logs
+# Prefix with MSYS_NO_PATHCONV=1 under windows if bash is mangling log group names
+alias aws_log_ls="aws logs describe-log-groups --query logGroups[].[logGroupName,storedBytes] --output table"
+alias aws_log_streams="aws logs describe-log-streams  --query logStreams[].[lastEventTimestamp,logStreamName] --output table --log-group-name"
+aws_log_cat() { aws logs get-log-events --log-group-name $1 --log-stream-name $2 --query events[].[message] --output text | sed '/^$/d'; }
+
+# Roles
+alias aws_rl_ls="aws iam list-roles --query Roles[].[RoleName] --output text"
+alias aws_rl_policies="aws iam list-role-policies --output text --query PolicyNames[].[@] --role-name"
+aws_rl_role_policy() { aws iam get-role-policy --role-name $1 --policy-name $2 --query PolicyDocument; }
