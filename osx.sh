d4d0840fb4c82a849319af97f35a558e935bdaf5
Andrew Taylor <ataylor@redtoad.ca>
2016-04-17 23:15:01 -0600

    Added MacOSX specific stuff.

---
+++ osx.sh
@@ -0,0 +1,8 @@
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock show-process-indicators -bool true
+defaults write com.apple.finder QuitMenuItem -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write NSGlobalDomain NSTextShowsControlCharacters -bool true
+chflags nohidden ~/Library
